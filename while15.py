3a0818c39cc26db84f05beb08b6736ed8adcd8a5
MarceloPrates0 <marcelo0610d@gmail.com>
2025-03-27 10:39:24 +0000

    2703

--- while15.py
+++ while15.py
@@ -17,42 +17,46 @@ Código  | Descrição
 """))
     match permissao:
         case 1: 
-            os.system("cls | clear")
-            idade = int(input("Insira sua idade: "))
+            os.system("cls || clear")
 
-        
+            # Idade 
+            idade = int(input("Insira sua idade: "))
             if idade < idade_menor :
                 idade_menor = idade
             if idade > idade_maior:
                 idade_maior = idade
             
+            # Salario 
             salario = float(input("Insira seu salário: "))
+            contador += 1
+            acumulador += salario
 
+            # Sexo
             sexo = input("""
-        Insira seu sexo:
-        F - Feminino
-        M - Masculino: 
-        """).upper
+Insira seu sexo:
+F - Feminino
+M - Masculino: 
+""").upper()
             match sexo:
                 case "M":
                     print()
                 case "F":
                     if salario > 4999:
                         mulheres_salario_mais5k += 1
+                case _:
+                    print("Opção inválida.")
 
-            
-            contador += 1
-            acumulador += salario
         case 2:
+            os.system("cls || clear")
             print(f"Sua idade: {idade}")
             print(f"Seu sexo: {sexo}")
             print(f"Seu salário: {salario}\n")
         case 3:
             break
+        case _:
+            print("Opção inválida.")
 
 media = acumulador / contador
-
-
-print(f"A média salarial desse grupo é: {media}")
-print(f"A maior idade do grupo é {idade_maior} e a menor idade é {idade_menor}")
+print(f"A média salarial desse grupo é: {media:.2f}")
+print(f"A menor idade é {idade_menor} e a maior idade do grupo é {idade_maior}")
 print(f"Possuem {mulheres_salario_mais5k} mulheres com o salário superior a R$5000")
