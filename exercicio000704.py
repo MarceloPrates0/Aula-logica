29c793acaebe0103e236fa1b0e180fc56e0d93e8
MarceloPrates0 <marcelo0610d@gmail.com>
2025-04-09 19:04:02 +0000

    0904

---
+++ exercicio000704.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls || clear")
+
+def nome(inserir_nome):
+    print(f"Olá, {inserir_nome}! Bem vindo(a) ao site.")
+
+nome_pessoa = input("Insira seu nome: ")
+nome(nome_pessoa)
